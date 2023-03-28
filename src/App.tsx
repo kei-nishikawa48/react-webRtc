53b4202b4efcfd1627b03b735b00d45642f35dcc
kei-nishikawa <xnkcelebrate48x@yahoo.co.jp>
2023-03-28 20:44:45 +0900

    tailwindcssのインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import "./index.css";
 const getMedia = async () => {
   const constraints: MediaStreamConstraints = { audio: true, video: true };
 
@@ -13,9 +13,18 @@ const getMedia = async () => {
 getMedia();
 const App = () => {
   return (
-    <div className="App">
-      <h1>webRTC</h1>
-    </div>
+    <main className="bg-slate-600 w-full h-screen">
+      <h1 className=" text-white text-xl">webRTC</h1>
+      <div className=" h-full text-center flex  justify-center items-center">
+        <div>
+          <label htmlFor="name">名前</label>
+          <input name="name" />
+        </div>
+        <button className=" rounded-sm bg-blue-400 px-3 py-1 hover:bg-blue-500">
+          test
+        </button>
+      </div>
+    </main>
   );
 };
 
